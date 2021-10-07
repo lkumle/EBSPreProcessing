70db92b2f74267ed06a95330859d87c238641b76
Leah Kumle <leahkumle@gmail.com>
2021-10-07 13:15:47 +1100

    fix duplicated rows error in final data

--- R/addSummaryVariables.R
+++ R/addSummaryVariables.R
@@ -3,60 +3,83 @@
 #' @export
 addSummaryVariables <- function(data){
   
+  before <- nrow(data) # how many entries do we have before?
+  
+  # remove duplicate rows in moodtracker data --> otherwise they are couted multiple times in summary statistics
+  data <- data %>%
+    dplyr::distinct_at(dplyr::vars(subID,moodTracker_timeStamp),.keep_all = T)  
+  
   # add new columns: 
   # --> if new aggregated variables are added, make sure to complete them her as well
   aggVariables <- c("completedTask", "NTasksSession", "firstUse", "lastUse",  "TimesUsageTotal", "NTasks", 
                     "N_2Back","N_affectiveSetShifting", "N_backwardDigitSpan", "N_emotionalStroop","N_nBack" )
   data[aggVariables] <- NA
   
-  for(sub in unique(data$subID)){
-    
-    subData <- data[data$subID == sub,]
-    
-
-    
-    # Did participant complete ANY task at this timepoint?
-    #check if any of the "completedTask" variables is TRUE (=1) --> if yes, mark as true
-    # first 5 "completed" variables are from tasks --> we don't want to count questionnaire completions
-    data$completedTask[subData$rowGlobal] <- ifelse(rowSums(dplyr::select(subData, dplyr::contains("completed"))[1:5]) > 0, T, F)
-    
-    # How many tasks did participant complete in one session?
-    data$NTasksSession[subData$rowGlobal] <- rowSums(dplyr::select(subData, dplyr::contains("completed"))[1:5])
-    
-    #How many times participants used the app (any usage)
-    data$TimesUsageTotal[subData$rowGlobal] <- nrow(subData)
-    
-    #How many times participants completed any task
-    data$NTasks[subData$rowGlobal] <- sum(rowSums(dplyr::select(subData, dplyr::contains("completed"))[1:5]))
-    
-    #How many times participants completed the ucl_2back
-    data$N_2Back[subData$rowGlobal] <- sum(subData$`2Back_completed`)
-    
-    #How many times participants completed the ucl_affectiveSetShifting
-    data$N_affectiveSetShifting[subData$rowGlobal] <- sum(subData$affectiveSetShifting_completed)
-    
-    #How many times participants completed the ucl_backwardDigitSpan
-    data$N_backwardDigitSpan[subData$rowGlobal] <- sum(subData$backwardDigitSpan_completed)
-    
-    #How many times participants completed the ucl_emotionalStroop
-    data$N_emotionalStroop[subData$rowGlobal] <- sum(subData$emotionalStroop_completed)
-    
-    #How many times participants completed the ucl_nBack
-    data$N_nBack[subData$rowGlobal] <- sum(subData$nBack_completed)
-    
-    #Date first & last use
-    TimeStamps <- as.vector(as.matrix(dplyr::select(subData, dplyr::contains("timeStamp")))) # put all timestamps into one vector
-    data$firstUse[subData$rowGlobal] <- min(TimeStamps, na.rm = T) # first use (smallest timestamp)
-    data$lastUse[subData$rowGlobal] <- max(TimeStamps, na.rm = T) # last use (smallest timestamp)
-    
+  data$rowGlobal <- 1:nrow(data)
   
+  for(sub in unique(data$subNo)){
     
-  }
+    
+    # break if unidentifiable subject.. we delete this later anyways and it makes everything break
+    if(sub != "unidentifiable"){
+      subData <- data[data$subNo == sub,]
+      
+      
+      
+      # Did participant complete ANY task at this timepoint?
+      #check if any of the "completedTask" variables is TRUE (=1) --> if yes, mark as true
+      # first 5 "completed" variables are from tasks --> we don't want to count questionnaire completions
+      data$completedTask[subData$rowGlobal] <- ifelse(rowSums(dplyr::select(subData, dplyr::contains("completed"))[1:5]) > 0, T, F)
+      
+      # How many tasks did participant complete in one session?
+      data$NTasksSession[subData$rowGlobal] <- rowSums(dplyr::select(subData, dplyr::contains("completed"))[1:5])
+      
+      #How many times participants used the app (any usage)
+      data$TimesUsageTotal[subData$rowGlobal] <- nrow(subData)
+      
+      
+      #How many times participants completed any task
+      data$NTasks[subData$rowGlobal] <- sum(rowSums(dplyr::select(subData, dplyr::contains("completed"))[1:5]))
+      
+      #How many times participants completed the ucl_2back
+      data$N_2Back[subData$rowGlobal] <- sum(subData$`2Back_completed`)
+      
+      #How many times participants completed the ucl_affectiveSetShifting
+      data$N_affectiveSetShifting[subData$rowGlobal] <- sum(subData$affectiveSetShifting_completed)
+      
+      #How many times participants completed the ucl_backwardDigitSpan
+      data$N_backwardDigitSpan[subData$rowGlobal] <- sum(subData$backwardDigitSpan_completed)
+      
+      #How many times participants completed the ucl_emotionalStroop
+      data$N_emotionalStroop[subData$rowGlobal] <- sum(subData$emotionalStroop_completed)
+      
+      #How many times participants completed the ucl_nBack
+      data$N_nBack[subData$rowGlobal] <- sum(subData$nBack_completed)
+      
+      #Date first & last use
+      TimeStamps <- as.vector(as.matrix(dplyr::select(subData, dplyr::contains("timeStamp")))) # put all timestamps into one vector
+      data$firstUse[subData$rowGlobal] <- min(TimeStamps, na.rm = T) # first use (smallest timestamp)
+      data$lastUse[subData$rowGlobal] <- max(TimeStamps, na.rm = T) # last use (smallest timestamp)
+      
+      
+    } # end if
+  } # emd for
   
-  # move new columns to front
+  
+  
+  
+  
   data <- data %>%
-    dplyr::select(c(subNo, subID, moodTracker_timeStamp, dplyr::all_of(aggVariables)), dplyr::everything()) %>%
-    dplyr::filter(subID != "unidentifiable") # get rid of all unidentifiable data points
+    dplyr::select(c(subNo, subID, moodTracker_timeStamp, dplyr::all_of(aggVariables)), dplyr::everything()) %>% # move new columns to front
+    dplyr::filter(subID != "unidentifiable") # get rid of all unidentifiable data points 
+  
+  
+  removed <- before - nrow(data)
+  print("---------------------------------------")
+  print("Removing duplicated entries and unidentifiable subjects")
+  print(paste( "--->" , removed, "data entires removed"))
+  
+  
   
   return(data)
   
