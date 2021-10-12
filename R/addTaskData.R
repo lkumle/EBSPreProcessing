0e36d7ac49a403d2e04991592ad5c91dfbbaca65
Leah Kumle <leahkumle@gmail.com>
2021-10-12 11:08:40 +1100

    remove duplicate entries in data + check if task was done before in same session (if yes, write in next row)

--- R/addTaskData.R
+++ R/addTaskData.R
@@ -22,6 +22,10 @@ addTaskData <- function(data,subDatabase, taskID, taskData, Q = F){
   data[paste0(taskID, "_timeStamp" )] <- as.POSIXct(NA) 
   data[paste0(taskID, "_completed")] <- FALSE # default of false (change to TRUE if task task was completed)
   
+  # AAA: remove duplicated entries in task data: --> there always are a few
+  taskData <- taskData %>% # -> remove if same subject and EXACTLY the same timestamp
+    dplyr::distinct_at(dplyr::vars(study_id, deviceModel,deviceUUID,timeStamp),.keep_all = T)  
+  
   # B: check how many colums data frame has by now (to know where to start indexing data later)
   colsBefore <- ncol(data) + 1 #new coumns should start one colum next to last column
   
@@ -44,6 +48,8 @@ addTaskData <- function(data,subDatabase, taskID, taskData, Q = F){
   
   task_columns <- names(taskData)[30:lastVariable] # use indexes provided
   data[task_columns] <- NA
+  
+
   
   colsAfter <- ncol(data) # check how many columns data frame has now
   
@@ -72,16 +78,56 @@ addTaskData <- function(data,subDatabase, taskID, taskData, Q = F){
     # find row in data that is clostest to timepoint of task data
     row <- matchTimepoint(data, tp, subID, taskData)
     
+    #print(row)
+    
     # count how many entries could not be matched
     if(length(row) == 0){sumNoMatch <- sumNoMatch + 1}
     
-    # C: add data  
-    data[row, paste0(taskID, "_timeStamp")] <- taskData$timeStamp[tp] # store timeStamp of task completed
-    data[row, paste0(taskID, "_completed")] <- T # set completed task to True
+    # ------------------------------- # 
+    # 4: add data
+    # ------------------------------- #
     
-    # add data from task to data frame
-    tp_data_task <- c(taskData[tp,30:lastVariable]) # get relevant variables of task data ()
-    data[row, colsBefore:colsAfter] <- tp_data_task # add to corresponding row in data (using column boundaries determined earlier)
+    # ---> check if task data was already matched to this timepoint
+    #      --> maybe subject did same task twice in one session!
+    if(length(row) != 0){ # --> data point did not match so we can't add it anyways!
+      row <- row[1]
+      if(data[row, paste0(taskID, "_completed")] == F){ # entry of moodtracker data is still empty! write in first row
+        
+        
+        # --> in this case: add data
+        data[row, paste0(taskID, "_timeStamp")] <- taskData$timeStamp[tp] # store timeStamp of task completed
+        data[row, paste0(taskID, "_completed")] <- T # set completed task to True
+        
+        # add data from task to data frame
+        tp_data_task <- c(taskData[tp,30:lastVariable]) # get relevant variables of task data ()
+        data[row, colsBefore:colsAfter] <- tp_data_task # add to corresponding row in data (using column boundaries determined earlier)
+        
+        
+      }else { # --> we already added data to this moodtracker entry! we need to find next empty row
+        
+
+        # go through next 5 rows and find first empty one --> we prepared those rows in addMoodtracker.R
+        for(i in 1:9){
+          
+          if(data[row + i, paste0(taskID, "_completed")] == F){ # check if row is empty
+            
+            # --> in this case: add data
+            data[row + i, paste0(taskID, "_timeStamp")] <- taskData$timeStamp[tp] # store timeStamp of task completed
+            data[row+i, paste0(taskID, "_completed")] <- T # set completed task to True
+            
+            # add data from task to data frame
+            tp_data_task <- c(taskData[tp,30:lastVariable]) # get relevant variables of task data ()
+            data[row+i, colsBefore:colsAfter] <- tp_data_task # add to corresponding row in data (using column boundaries determined earlier)
+          
+            break # stop ging through rows 
+          }
+          
+        } # end for
+      }#end # if else
+    }# end if row == 0
+    
+    
+    
     
     
   } # end tp
